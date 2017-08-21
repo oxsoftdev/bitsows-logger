b3d2e979c7b28700cc8122e308fbdfe5b9a0d342
oxsoftdev <oxsoftdev@gmail.com>
2017-08-21 15:09:31 -0700

    fix(platform): windows

--- logger.py
+++ logger.py
@@ -10,12 +10,13 @@ from lib.subscribers import SimpleLoggerSubscriber
 logging.config.dictConfig(lib.configs.logging.d)
 
 
-books = Api().available_books().books
-with Websocket(books) as client:
-    with SimpleLoggerSubscriber(client):
-        client.connect()
-        try:
-            tornado.ioloop.IOLoop.instance().start()
-        except KeyboardInterrupt:
-            client.close()
+if __name__ == '__main__':
+    books = Api().available_books().books
+    with Websocket(books) as client:
+        with SimpleLoggerSubscriber(client):
+            client.connect()
+            try:
+                tornado.ioloop.IOLoop.instance().start()
+            except KeyboardInterrupt:
+                client.close()
 
